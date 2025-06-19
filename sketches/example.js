b9bfe2df403015e1f1d3065ffbe2a6b6f26af0b0
seeun0713 <139195292+seeun0713@users.noreply.github.com>
2025-06-20 06:57:42 +0900

    first edit

---
+++ sketches/example.js
@@ -0,0 +1,84 @@
+const { Responsive } = P5Template;
+
+let faceapi;
+let detections = [];
+
+let video;
+let surprise = 0;
+
+function setup() {
+  new Responsive().createResponsiveCanvas(800, 600, 'contain', true);
+  video = createCapture(VIDEO);
+  video.size(64, 48);
+  video.hide();
+
+  const options = {
+    withLandmarks: false,
+    withExpressions: true,
+    withDescriptors: false,
+  };
+
+  faceapi = ml5.faceApi(video, options, modelReady);
+}
+
+function modelReady() {
+  console.log('FaceAPI ready!');
+  faceapi.detect(gotFaces);
+}
+
+function gotFaces(error, result) {
+  if (error) {
+    console.error(error);
+    return;
+  }
+
+  detections = result;
+
+  if (detections.length > 0) {
+    let expr = detections[0].expressions;
+    surprise = expr.surprised || 0;
+  } else {
+    surprise = 0;
+  }
+
+  faceapi.detect(gotFaces);
+}
+
+function draw() {
+  background('#000000');
+  noStroke();
+
+  Responsive.drawReferenceGrid('#ffffff');
+
+  video.loadPixels();
+
+  let baseWidth = width / video.width;
+  let baseHeight = height / video.height;
+
+  let tileWidth, tileHeight;
+  if (surprise > 0.5) {
+    tileWidth = baseWidth * 1.5;
+    tileHeight = baseHeight * 1.5;
+  } else {
+    tileWidth = baseWidth;
+    tileHeight = baseHeight;
+  }
+
+  for (let idx = 0; idx < video.pixels.length / 4; idx++) {
+    let column = idx % video.width; // 열 위치
+    let row = floor(idx / video.width); // 행 위치
+
+    let r = video.pixels[idx * 4];
+    let g = video.pixels[idx * 4 + 1];
+    let b = video.pixels[idx * 4 + 2];
+    let a = video.pixels[idx * 4 + 3];
+
+    if (surprise > 0.5) {
+      fill(255 - r, 255 - g, 255 - b, a);
+    } else {
+      fill(r, g, b, a);
+    }
+
+    rect(column * tileWidth, row * tileHeight, tileWidth, tileHeight);
+  }
+}
